db3d97b24731374e7317d63d55b0de23c8caa583
Gustavo Medeiros <gugamedeiros517@gmail.com>
2022-09-29 16:34:07 -0300

    feat: adiciona método de criação de Product no service

---
+++ src/products/products.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from './product.model';
+
+@Injectable()
+export class ProductsService {
+  products: Product[] = [];
+
+  insertProduct(title: string, description: string, price: number) {
+    const newProduct = new Product(
+      new Date().toString(),
+      title,
+      description,
+      price,
+    );
+
+    this.products.push(newProduct);
+  }
+}
